c1e01357a35049286ceecad801f65df781c68206
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-06-30 08:24:27 +0200

    Update format_menu.py

--- format_menu.py
+++ format_menu.py
@@ -1,4 +1,13 @@
-from imports.imports import *
+from tkinter import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+from tkinter.simpledialog import *
+from tkinter.ttk import *
+from tkinter.font import Font, families
+from tkinter.colorchooser import askcolor
+from tkinter.scrolledtext import *
+from tkinter import Scrollbar, Text, messagebox, Menu
+import time
 
 class Format(): #Format menu
     def __init__(self, text):
