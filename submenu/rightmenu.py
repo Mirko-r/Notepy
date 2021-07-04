b66023453f6a99f94861a2e85889d9228d77bef5
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-07-04 11:27:42 +0200

    Add files via upload

---
+++ submenu/rightmenu.py
@@ -0,0 +1,32 @@
+from tkinter import Menu, messagebox
+from submenu import format_menu, edit_menu, revision_menu
+
+def main(root, text, menubar, status_bar):
+    edit_menu.main(root, text, menubar, status_bar)
+    objEdit = edit_menu.Edit(text, root, status_bar)
+
+    format_menu.main(root, text, menubar, status_bar)
+    objFormat = format_menu.Format(text)
+
+    revision_menu.main(root, text, menubar, status_bar)
+    objRevision = revision_menu.Revision(text, status_bar)
+
+    m = Menu(root, tearoff = 0) #menu rightclick
+    m.add_command(label ="Cut", command=objEdit.cut)
+    m.add_command(label ="Copy", command=objEdit.copy)
+    m.add_command(label ="Paste", command=objEdit.paste)
+    m.add_command(label="Find", command=objEdit.find)
+    m.add_command(label="Add Date", command=objFormat.addDate)
+    m.add_command(label="Add Hour", command=objFormat.addHour)
+    m.add_command(label="Search on internet", command=objRevision.open_webb)
+
+    def do_popup(event):
+        try:
+            m.tk_popup(event.x_root, event.y_root)
+        finally:
+            m.grab_release()
+
+    text.bind("<Button-3>", do_popup)
+
+if __name__ == "__main__":
+    messagebox.showerror("Eror", "Please run 'main.py'")
