41d8c3922707a50b1050fb0a726c4d938d9b2cda
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-07-05 08:24:13 +0200

    Correct an error

--- submenu/revision_menu.py
+++ submenu/revision_menu.py
@@ -42,7 +42,7 @@ class Revision(): # Revision menu
             a = self.text.get(SEL_FIRST, SEL_LAST)
             if self.text.tag_ranges(SEL):
                 b = TextBlob(a)
-                corrected_text = str(b.correct(),)
+                corrected_text = str(b.correct())
                 entry = askyesno(title="Correction", message="Suggest corrections :\n\n" + corrected_text + "\n\nContinue?")
                 if entry == True:
                     self.text.delete(1.0, END)
