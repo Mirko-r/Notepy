cf7a9e41cbd4c0f0923b802a4912bfe31756b02a
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-07-10 10:15:12 +0200

    Update notepy.py

--- notepy.py
+++ notepy.py
@@ -9,53 +9,57 @@ from submenu import file_menu, format_menu, edit_menu, revision_menu, help_menu,
 import os
 from sys import platform
 
-def run_terminal(*args):
-    if platform == 'darwin':
-        os.system('open -a Terminal -n')
-    elif platform == 'win32':
-        os.system('start cmd')
-    elif platform == 'linux':
-        os.system('gnome-terminal')
 
-root = Tk()
+def main():
+    def run_terminal(*args):
+        if platform == 'darwin':
+            os.system('open -a Terminal -n')
+        elif platform == 'win32':
+            os.system('start cmd')
+        elif platform == 'linux':
+            os.system('gnome-terminal')
 
-root.title("Notepy")
-root.geometry("1200x600")
-root.minsize(width=600, height=600)
-root.iconbitmap("icon/notepy.ico")
-status_bar = Label(root, text = 'Ready   ', anchor = E)
-status_bar.pack( fill = "x", side = "bottom", ipady = 4 ) 
+    root = Tk()
 
-scrollbar = Scrollbar(root)
-scrollbar.pack( side = RIGHT, fill = Y)
+    root.title("Notepy")
+    root.geometry("1200x600")
+    root.minsize(width=600, height=600)
+    root.iconbitmap("icon/notepy.ico")
+    status_bar = Label(root, text='Ready   ', anchor=E)
+    status_bar.pack(fill="x", side="bottom", ipady=4)
 
-text = Text(root, state='normal', width=400, height=400, wrap='word', pady=2, padx=3, undo=True, selectbackground = "yellow", selectforeground = "black",)
-text.pack(fill=Y, expand=1)
-text.config(yscrollcommand= scrollbar.set)
-text.focus_set()
-scrollbar.config(command = text.yview)
 
-menubar = Menu(root)
+    scrollbar = Scrollbar(root)
+    scrollbar.pack(side=RIGHT, fill=Y)
 
-file_menu.main(root, text, menubar, status_bar)
+    text = Text(root, state='normal', width=400, height=400, wrap='word', pady=2,
+            padx=3, undo=True, selectbackground="yellow", selectforeground="black",)
+    text.pack(fill=Y, expand=1)
+    text.config(yscrollcommand=scrollbar.set)
+    text.focus_set()
+    scrollbar.config(command=text.yview)
 
-edit_menu.main(root, text, menubar, status_bar)
+    menubar = Menu(root)
 
-format_menu.main(root, text, menubar, status_bar)
+    file_menu.main(root, text, menubar, status_bar)
 
-revision_menu.main(root, text, menubar, status_bar)
+    edit_menu.main(root, text, menubar, status_bar)
 
-rightmenu.main(root, text, menubar, status_bar)
+    format_menu.main(root, text, menubar, status_bar)
 
-menubar.add_command(label="Run terminal", command=run_terminal)
+    revision_menu.main(root, text, menubar, status_bar)
 
-help_menu.main(root, text, menubar)
+    rightmenu.main(root, text, menubar, status_bar)
 
-Percolator(text).insertfilter(ColorDelegator())
+    menubar.add_command(label="Run terminal", command=run_terminal)
 
-root.grid_columnconfigure(0, weight=1)
-root.resizable(True, True)
+    help_menu.main(root, text, menubar)
 
-root.bind_all("<Control-t>", run_terminal)
+    Percolator(text).insertfilter(ColorDelegator())
 
-root.mainloop()
+    root.grid_columnconfigure(0, weight=1)
+    root.resizable(True, True)
+
+    root.bind_all("<Control-t>", run_terminal)
+
+    root.mainloop()
