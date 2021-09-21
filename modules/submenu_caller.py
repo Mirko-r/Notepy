badd110b813593899b56cad4902acbc7accbd8a8
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-09-21 15:16:56 +0200

    Add files via upload

---
+++ modules/submenu_caller.py
@@ -0,0 +1,18 @@
+from submenu import file_menu, format_menu, edit_menu, revision_menu, code_menu, help_menu, rightmenu, run_menu
+
+def main(root, text, menubar, status_bar):
+    file_menu.main(root, text, menubar, status_bar)
+
+    edit_menu.main(root, text, menubar, status_bar)
+
+    format_menu.main(root, text, menubar, status_bar)
+
+    revision_menu.main(root, text, menubar, status_bar)
+
+    code_menu.main(root, text, menubar, status_bar)
+    
+    rightmenu.main(root, text, menubar, status_bar)
+    
+    help_menu.main(root, menubar)
+
+    run_menu.main(root, menubar)
