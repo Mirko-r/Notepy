865b5f928f5dab53c1ebbc6a19b27cb904d81a46
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-09-26 20:43:29 +0200

    Update and rename status_bar_line_and_column.py to status_bar_.py

--- modules/status_bar_.py
+++ modules/status_bar_.py
@@ -7,7 +7,7 @@ def main(text, status_bar):
     def get_position(event ):
         """get the line and column number of the text insertion point"""
         line, column = text.index('insert').split('.')
-        s = "line=%s  column=%s" % (line, column)
+        s = "line=%s" % (line)
         status_bar.config(text = s + "  ")
 
     text.bind("<KeyRelease>", get_position)
