5305b010f24f39a699f7e440423c3acfab98d885
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-09-27 08:41:52 +0200

    Add chars and words counter

--- modules/status_bar_.py
+++ modules/status_bar_.py
@@ -4,11 +4,31 @@ from tkinter import *
 
 def main(text, status_bar):
 
-    def get_position(event ):
-        """get the line and column number of the text insertion point"""
-        line, column = text.index('insert').split('.')
-        s = "line=%s" % (line)
-        status_bar.config(text = s + "  ")
+    def update(event ):
 
-    text.bind("<KeyRelease>", get_position)
+        try:
+            line, column = text.index('insert').split('.')
+        except:
+            pass
 
+        try:
+            t = text.get(0.0, END)
+            words = [w for w in t.split(' ') if w!='\n']
+            word_count = len(words)
+        except:
+            pass
+
+        try:
+            text_var = text.get(1.0, END)
+            word_list = text_var.split('\n')
+            count = 0
+            for x in word_list:
+                if x != "":
+                    count += len(x)
+        except:
+            pass
+
+        s = "| chars=%s | words=%s | line=%s  " % (count, word_count, line)
+        status_bar.config(text = s)
+
+    text.bind("<KeyRelease>", update)
