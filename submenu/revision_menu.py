621f73fbdea706c5960d97e300a2f582133d4bc2
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-09-27 08:41:17 +0200

    Remove count words and chars

--- submenu/revision_menu.py
+++ submenu/revision_menu.py
@@ -11,22 +11,7 @@ class Revision(): # Revision menu
     def __init__(self, text, status_bar):
         self.text = text
         self.status_bar = status_bar
-
-    def words_count(self):
-        t = self.text.get(0.0, END)
-        words = [w for w in t.split(' ') if w!='\n']
-        word_count = len(words)
-        messagebox.showinfo("Word Count", "word count: {:,d}".format(word_count))
     
-    def chars_count(self):
-        text_var = self.text.get(1.0, END)
-        word_list = text_var.split('\n')
-        count = 0
-        for x in word_list:
-            if x != "":
-                count += len(x)
-        messagebox.showinfo("Character Count ", "characters count: {:,d}".format(count))
-
     def readtime(self):
         result = str(readtime.of_text(self.text.get(0.0, END)))
         messagebox.showinfo("Read Time", "Time :" + result)
@@ -100,9 +85,6 @@ class Revision(): # Revision menu
 def main(root, text, menubar, status_bar):
     revisionmenu = Menu(menubar, tearoff=False) ## Revision menu gui
     objRevision = Revision(text, status_bar)
-    revisionmenu.add_command(label="Count Words", command=objRevision.words_count)
-    revisionmenu.add_command(label="Count Characters", command=objRevision.chars_count)
-    revisionmenu.add_separator()
     revisionmenu.add_command(label="Calculate read time", command=objRevision.readtime)
     revisionmenu.add_separator()
     revisionmenu.add_command(label="Check if number is palindrome", command=objRevision.check_palindrome)
